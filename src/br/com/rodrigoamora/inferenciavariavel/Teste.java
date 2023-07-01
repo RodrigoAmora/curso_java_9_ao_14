7d1a9fb006a5e0b520b15d5b7542e725ca8f4822
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-01 05:57:15 -0300

    Java 10 - Inferência de variável

--- src/br/com/rodrigoamora/inferenciavariavel/Teste.java
+++ src/br/com/rodrigoamora/inferenciavariavel/Teste.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 
 public class Teste {
 
+	// Novidades do Java 10 - Inferência de variável
 	public static void main(String[] args) {
+		// A palavra var só pode ser usada para definir variáveis locais
 		var cpfPorNomes = new HashMap<String, String>();
 		cpfPorNomes.put("Mario", "04813189");
 
