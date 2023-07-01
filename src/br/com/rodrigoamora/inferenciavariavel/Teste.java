cb99c81e40bff94b7f5ecb2c0750c76e436e0c28
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-01 05:55:55 -0300

    Java 10 - Inferência de variável

--- src/br/com/rodrigoamora/inferenciavariavel/Teste.java
+++ src/br/com/rodrigoamora/inferenciavariavel/Teste.java
@@ -1,5 +1,14 @@
 package br.com.rodrigoamora.inferenciavariavel;
 
+import java.util.HashMap;
+
 public class Teste {
 
+	public static void main(String[] args) {
+		var cpfPorNomes = new HashMap<String, String>();
+		cpfPorNomes.put("Mario", "04813189");
+
+		System.out.println(cpfPorNomes);
+	}
+	
 }
