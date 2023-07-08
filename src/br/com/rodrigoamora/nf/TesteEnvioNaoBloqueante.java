549ea7eac57bcdf0f68c2865514ac8be300572a4
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-07-08 02:14:27 -0300

    Verificando quantas Threads há

--- src/br/com/rodrigoamora/nf/TesteEnvioNaoBloqueante.java
+++ src/br/com/rodrigoamora/nf/TesteEnvioNaoBloqueante.java
@@ -2,6 +2,8 @@ package br.com.rodrigoamora.nf;
 
 import java.time.LocalDate;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.SubmissionPublisher;
 
 import br.com.rodrigoamora.model.NotaFiscal;
@@ -10,10 +12,15 @@ import br.com.rodrigoamora.wsclient.NotaFiscalWSClient;
 public class TesteEnvioNaoBloqueante {
 
 	public static void main(String[] args) {
+		ExecutorService a = Executors.newFixedThreadPool(2);
+		System.out.println("A thread do TesteEnvioNaoBloqueante é: " + Thread.currentThread().getName());
 		NotaFiscal primeiraNotaFiscal = new NotaFiscal("João", 39.99, LocalDate.now());
 		SubmissionPublisher<NotaFiscal> publisher = new SubmissionPublisher<>();
 		NotaFiscalWSClient nfwsc = new NotaFiscalWSClient();
 		publisher.consume(nfwsc::enviar);
+		publisher.consume(data -> {
+			System.out.println("Oura thread: " + Thread.currentThread().getName());
+		});
 		publisher.submit(primeiraNotaFiscal);
 		System.out.println("Voc� ir� receber a nota fiscal no seu e-mail");
 		Scanner scan = new Scanner(System.in);
